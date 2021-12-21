7be82ac543e996dc0d63dd9e5853b0e59b7e7784
Aaron Lu <aaronnoh0409@gmail.com>
2021-12-20 21:19:43 -0500

    added routes table on readme and also added empty routes in places.js

--- controllers/places.js
+++ controllers/places.js
@@ -4,4 +4,39 @@ router.get('/', (req, res) => {
     res.send('GET /places')
 })
 
+router.get('/places', (req, res) => {
+    res.send('GET /placesPlaces')
+})
+
+router.post('/places', (req, res) => {
+})
+
+router.get('/places/new', (req, res) => {
+    res.send('GET /places/new')
+})
+
+router.get('/places/:id', (req, res) => {
+    res.send('GET /places/id')
+})
+
+router.put('/places/:id', (req, res) => {
+})
+
+router.get('/places/:id/edit', (req, res) => {
+    res.send('GET /places/id/eidt')
+})
+
+router.delete('/places/:id', (req, res) => {
+})
+
+router.post('/places/:id/rant', (req, res) => {
+})
+
+router.delete('/places/:id/rant/:rantId', (req, res) => {
+})
+
+router.get('*', (req, res) => {
+    res.send('haloWorld')
+})
+
 module.exports = router
