f46aaf2914e7d246058d8957ad92ce38190e9dcf
Aaron Lu <aaronnoh0409@gmail.com>
2021-12-22 16:34:20 -0500

    added view for places

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    res.render('places/index')
 })
 
 router.get('/places', (req, res) => {
---
+++ views/places/index.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('../default')
+
+function main() {
+    return (
+        <Def>
+            <main>
+                <h1>MAIN</h1>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = main
