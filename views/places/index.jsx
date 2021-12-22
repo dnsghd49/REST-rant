aab29d9ccc64bde6ba6dd8d7a09143f0fe92afe3
Aaron Lu <aaronnoh0409@gmail.com>
2021-12-22 16:41:51 -0500

    added some mock data to display some content into places view

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,14 +1,23 @@
 const React = require('react')
 const Def = require('../default')
 
-function main() {
+function places(data) {
+    let placesFormatted = data.places.map((place) => {
+        return (
+            <div>
+                <h2>{place.name}</h2>
+                <img src={place.pic} alt={place.name} />
+            </div>
+        )
+    })
     return (
         <Def>
             <main>
-                <h1>MAIN</h1>
+                <h1>PLACES INDEX PAGE</h1>
+                {placesFormatted}
             </main>
         </Def>
     )
 }
 
-module.exports = main
+module.exports = places
