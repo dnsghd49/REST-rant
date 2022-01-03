cb88ad21285422b06b20636471b2ee48bcfe2c50
Aaron Lu <aaronnoh0409@gmail.com>
2022-01-03 15:59:53 -0500

    bootstrap added

--- views/default.jsx
+++ views/default.jsx
@@ -5,9 +5,13 @@ function Def(html) {
         <html>
             <head>
                 <title>Title</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
+                <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
                 {html.children}
+
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
             </body>
         </html>
     )
