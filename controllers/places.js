8be80ea2adb9686d55e44726c79144c2553f7b35
Aaron Lu <aaronnoh0409@gmail.com>
2022-01-03 15:24:25 -0500

    check point to add new places

--- controllers/places.js
+++ controllers/places.js
@@ -1,19 +1,7 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-    }]
     res.render('places/index', { places })
 })
 
@@ -21,7 +9,19 @@ router.get('/places', (req, res) => {
     res.send('GET /placesPlaces')
 })
 
-router.post('/places', (req, res) => {
+router.post('/', (req, res) => {
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
 
 router.get('/new', (req, res) => {
