eb2ca368f8a9b623803a44e7729079b84ab8d24b
Aaron Lu <aaronnoh0409@gmail.com>
2022-01-03 15:24:40 -0500

    check point to add new places

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const places = require('../models/places.js')
+const places = require('../models/places')
 
 router.get('/', (req, res) => {
     res.render('places/index', { places })
