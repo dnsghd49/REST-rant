e29fa6af6f90d19c7c88d6038091133e5ed325c9
Ayron's Macbook <ayayron@Ayrons-MacBook-Pro.local>
2022-01-21 16:50:01 -0500

    comments added on show page

--- controllers/places.js
+++ controllers/places.js
@@ -30,7 +30,9 @@ router.post('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     db.Place.findById(req.params.id)
+        .populate('comments')
         .then(place => {
+            console.log(place.comments)
             res.render('places/show', { place })
         })
         .catch(err => {
@@ -50,6 +52,27 @@ router.get('/:id/edit', (req, res) => {
     else {
         res.render('places/edit', { place: places[id], id })
     }
+})
+
+router.post('/:id/comment', (req, res) => {
+    console.log(req.body)
+    db.Place.findById(req.params.id)
+        .then(place => {
+            db.Comment.create(req.body)
+                .then(comment => {
+                    place.comments.push(comment.id)
+                    place.save()
+                        .then(() => {
+                            res.redirect(`/places/${req.params.id}`)
+                        })
+                })
+                .catch(err => {
+                    res.render('error404')
+                })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
 })
 
 router.put('/:id', (req, res) => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show(data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <stong>- {c.author}</stong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <main>
@@ -12,6 +31,7 @@ function show(data) {
                 <h2>Description</h2>
                 <h3>{data.place.showEstablished()}</h3>
                 <h4>Serving {data.place.cuisines}</h4>
+                <h2>Comments {comments}</h2>
 
                 {/* need to show more stuff on this view */}
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning">
