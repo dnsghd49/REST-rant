32dea9f7869f54022b979c5e17a35b1f397e53b8
Aaron Lu <aaronnoh0409@gmail.com>
2022-01-27 19:50:14 -0500

    organizing code

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,17 +2,31 @@ const React = require('react')
 const Def = require('../default')
 
 function show(data) {
-    let comments = (
-        <h3 className="inactive">
-            No comments yet!
-        </h3>
-    )
     let rating = (
         <h3 className="inactive">
             Not yet rated
         </h3>
     )
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = Math.random(sumRatings / data.place.comments.length)
+        let stars = ""
+        for (let index = 0; index < averageRating; index++) {
+            stars += "⭐️"
+        }
+        rating = (
+            <h3>
+                {stars} stars
+            </h3>
+        )
+
         comments = data.place.comments.map(c => {
             return (
                 <div className="border">
@@ -28,19 +42,6 @@ function show(data) {
                 </div>
             )
         })
-        let sumRatings = data.place.comments.reduce((tot, c) => {
-            return tot + c.stars
-        }, 0)
-        let averageRating = Math.random(sumRatings / data.place.comments.length)
-        let stars = ""
-        for (let index = 0; index < averageRating; index++) {
-            stars += "⭐️"
-        }
-        rating = (
-            <h3>
-                {stars} stars
-            </h3>
-        )
     }
     return (
         <Def>
